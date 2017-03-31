79a7acde1473c9729df427324190986633d605a8
hafizh <hafizzaturrahim@gmail.com>
2017-03-31 08:29:59 +0700

    v 1.0

--- app/src/main/java/com/hafizzaturrahim/monitoringgilingan/grafik/GraphFragment.java
+++ app/src/main/java/com/hafizzaturrahim/monitoringgilingan/grafik/GraphFragment.java
@@ -1,19 +1,45 @@
 package com.hafizzaturrahim.monitoringgilingan.grafik;
 
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.hafizzaturrahim.monitoringgilingan.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GraphFragment extends Fragment {
+public class GraphFragment extends Fragment implements View.OnClickListener {
+    //UI References
+    private EditText fromDateEtxt;
+    private EditText toDateEtxt;
 
+    private DatePickerDialog fromDatePickerDialog;
+    private DatePickerDialog toDatePickerDialog;
+
+    private SimpleDateFormat dateFormatter;
+
+    // 0 = parameter
+    // 1 = tanggal mulai
+    // 2 = tanggal berakhir
+    // 3 = jam mulai
+    // 4 = jam berakhir
+    String[] selectedInput = new String[5];
 
     public GraphFragment() {
         // Required empty public constructor
@@ -24,7 +50,71 @@ public class GraphFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_graph, container, false);
+        View rowView = inflater.inflate(R.layout.fragment_graph, container, false);
+
+        Spinner spinnerParam = (Spinner) rowView.findViewById(R.id.spParam);
+        spinnerParam.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // your code here
+                selectedInput[0] = parentView.getItemAtPosition(position).toString();
+//                Toast.makeText(getActivity(), "The planet is " +
+//                        selectedInput[0], Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+
+
+
+        fromDateEtxt = (EditText) rowView.findViewById(R.id.etxt_fromdate);
+        fromDateEtxt.setInputType(InputType.TYPE_NULL);
+        fromDateEtxt.requestFocus();
+
+        toDateEtxt = (EditText) rowView.findViewById(R.id.etxt_todate);
+        toDateEtxt.setInputType(InputType.TYPE_NULL);
+
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+        setDateTimeField();
+        return rowView;
     }
 
+    private void setDateTimeField() {
+        fromDateEtxt.setOnClickListener(this);
+        toDateEtxt.setOnClickListener(this);
+
+        Calendar newCalendar = Calendar.getInstance();
+        fromDatePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                fromDateEtxt.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        toDatePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                toDateEtxt.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == fromDateEtxt) {
+            fromDatePickerDialog.show();
+        } else if (v == toDateEtxt) {
+            toDatePickerDialog.show();
+        }
+    }
 }
