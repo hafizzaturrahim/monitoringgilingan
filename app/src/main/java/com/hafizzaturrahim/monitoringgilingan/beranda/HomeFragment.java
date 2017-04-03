1cfc2dcdc013a5c957a5a48eca07c77b5e627b64
hafizh <hafizzaturrahim@gmail.com>
2017-04-03 17:21:44 +0700

    v 1.0

--- app/src/main/java/com/hafizzaturrahim/monitoringgilingan/beranda/HomeFragment.java
+++ app/src/main/java/com/hafizzaturrahim/monitoringgilingan/beranda/HomeFragment.java
@@ -1,20 +1,31 @@
 package com.hafizzaturrahim.monitoringgilingan.beranda;
 
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.hafizzaturrahim.monitoringgilingan.R;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
-
+    private ProgressDialog pDialog;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -25,7 +36,37 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rowView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        pDialog = new ProgressDialog(getActivity());
+
+        requestData();
+        return rowView;
+
     }
 
+    private void requestData() {
+
+        pDialog.setMessage("Memproses Data...");
+        pDialog.show();
+        /*Json Request*/
+        String url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Malang&units=metric&cnt=6&APPID=a4f161b4c56a2e0a250cf9c5ecf892b8";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        pDialog.dismiss();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+        //add request to queue
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        requestQueue.add(jsonObjectRequest);
+
+    }
 }
